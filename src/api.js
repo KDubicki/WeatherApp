dbcde7582961b61d885204759fd2e27d69796fa0
KDubicki <kamil3234@gmail.com>
2022-07-20 22:11:41 +0200

    Create Search component

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'ed0890c89bmshd524df86b9b3fffp132f5ejsn5896b04c8f0a',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
