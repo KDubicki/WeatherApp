a7a31ef5669c510c4be02a50d507f72de23468ec
KDubicki <kamil3234@gmail.com>
2023-03-07 19:15:37 +0100

    Forecast component changes

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,5 +1,39 @@
-const forecast = () => {
-    return '';
+import "./forecast.css";
+
+const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+const Forecast = ({ data }) => {
+    const dayInAWeek = new Date().getDay();
+    const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek));
+
+    return (
+        <div className="forecast">
+             {data.list.splice(0, 6).map((item, idx) => (
+                <div className="forecast__box" key={idx}>
+                    <img 
+                        src={`icons/${item.weather[0].icon}.png`}
+                        alt="weather icon" 
+                        className="forecast__icon"
+                    />
+                    <div className="forecast__info">
+                        <h3 className="forecast__day">{forecastDays[idx]}</h3>
+                        <div className="parameter">
+                            <div className="parameter__box">
+                                <span className="parameter__value">
+                                    {Math.round(item.main.temp_max)}°C / {Math.round(item.main.temp_min)}°C
+                                </span>
+                                <span className="parameter__label">Temperature</span>
+                            </div>
+                            <div className="parameter__box">
+                                <span className="parameter__value">{item.main.humidity}%</span>
+                                <span className="parameter__label">Humidity</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+             ))}
+        </div>
+    );
 }
 
-export default forecast;
+export default Forecast;
