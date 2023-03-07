5247ebfd935ea8abd0b13c2bfc5cb1f8e4d988a3
KDubicki <kamil3234@gmail.com>
2023-03-07 19:14:36 +0100

    some changes for application

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': 'ed0890c89bmshd524df86b9b3fffp132f5ejsn5896b04c8f0a',
+        'X-RapidAPI-Key': '',
         'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
     }
 };
